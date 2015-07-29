78c57eb43f755413f46484eb078d59b6027a8a53
Daria <daria@Freerunningtechnologies.com>
2015-07-29 10:29:16 -0700

    Add methods for checking the Sitemap file
    Check if there is a link to the sitemap.xml file and wether the file is
    empty

--- robots_file.rb
+++ robots_file.rb
@@ -10,10 +10,15 @@ class RobotsFile
   end
 
   def has_sitemap?
-    !!(get.body =~ /.*Sitemap: https?:\/\/.*\/sitemap.xml(.gz)?/)
+    !sitemap_string.nil?
+  end
+
+  def sitemap_is_empty?
+    download_sitemap.nil?
   end
 
   private
+
   attr_reader :uri
 
   def set_path
@@ -22,5 +27,17 @@ class RobotsFile
 
   def get
     Net::HTTP.get_response(uri)
+  end
+
+  def sitemap_string
+    get.body[/.*Sitemap: https?:\/\/.*\/sitemap.xml(.gz)?$/]
+  end
+
+  def sitemap_url
+    sitemap_string.match(/Sitemap:\s*(.*sitemap.xml(.gz)?)$/)[1]
+  end
+
+  def download_sitemap
+    open(sitemap_url).read
   end
 end
--- test.rb
+++ test.rb
@@ -8,5 +8,5 @@ puts "Redirects to www: #{request.redirects_to_www?}"
 puts "Redirects to https: #{request.redirects_to_https?}"
 
 request2 = RobotsFile.new(url)
-puts request2.exists?
-puts request2.has_sitemap?
+puts "Robots.txt exists: #{request2.exists?}"
+puts "Has a link to the Sitemap: #{request2.has_sitemap?} and Sitemap file is empty: #{request2.sitemap_is_empty?}"
